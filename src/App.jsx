cf04938510ca2c49bbdd0f5c3dbd36a696b12d80
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-05-11 14:18:22 +0100

    index.html, App.jsx, Main.jsx, Navbar.jsx, index.css, Navbar.jsx
    changed the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+
+
 import React from "react"
 import Navbar from "./components/Navbar.jsx"
 import Main from "./components/Main.jsx"
 
-function App() {
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+    
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                darkMode={darkMode} 
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main darkMode={darkMode} />
         </div>
     )
 }
-
-export default App
